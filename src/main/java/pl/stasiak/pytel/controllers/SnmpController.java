21d422855e37dc97315c3912645b93e6a19a7a7d
Kamil <kkamilstasiakk@gmail.com>
2016-12-11 15:31:25 +0100

    sadas

--- src/main/java/pl/stasiak/pytel/controllers/SnmpController.java
+++ src/main/java/pl/stasiak/pytel/controllers/SnmpController.java
@@ -18,6 +18,8 @@ import java.util.List;
 /**
  * Created by Kamil on 10.12.2016.
  */
+
+//localhost:8080/snmp/get/127.0.0.1/.1.3.6.1.2.1.4.20.1.2.192.168.0.12/
 @Controller
 @RequestMapping("snmp")
 public class SnmpController {

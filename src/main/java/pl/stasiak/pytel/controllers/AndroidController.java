5c6b364e3f16ce990b4918fa462b04ca6c720c02
kamilstasiakk <kkamilstasiakk@gmail.com>
2017-01-07 11:57:35 +0100

    Add files via upload

---
+++ src/main/java/pl/stasiak/pytel/controllers/AndroidController.java
@@ -0,0 +1,118 @@
+package pl.stasiak.pytel.controllers;
+
+import javafx.util.Pair;
+import org.snmp4j.smi.OID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.stasiak.pytel.SnmpManager;
+import pl.stasiak.pytel.entities.AndroidGetReply;
+import pl.stasiak.pytel.entities.GetReply;
+import pl.stasiak.pytel.entities.GetTableReply;
+import pl.stasiak.pytel.entities.TableRecord;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kamil on 07.01.2017.
+ */
+@Controller
+@RequestMapping("android")
+public class AndroidController {
+    SnmpManager client;
+    public AndroidController()  {
+        client = new SnmpManager("udp:127.0.0.1/161");
+    }
+
+    @RequestMapping(value = "/get/SysUpTime", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody ResponseEntity<AndroidGetReply> getSysUpTime() {
+
+        try {
+            String value = client.getAsString(new OID(".1.3.6.1.2.1.25.1.1.0"));
+            return new ResponseEntity<AndroidGetReply>(new AndroidGetReply(value, "System up time"), HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<AndroidGetReply>(new AndroidGetReply("", ""), HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "/get/usersCount", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody ResponseEntity<AndroidGetReply> getUsersCount() {
+
+        try {
+            String value = client.getAsString(new OID(".1.3.6.1.2.1.25.1.5.0"));
+            return new ResponseEntity<AndroidGetReply>(new AndroidGetReply(value, "System users count"), HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<AndroidGetReply>(new AndroidGetReply("", ""), HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "/get/TcpConnections", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody ResponseEntity<GetTableReply> getTcpConnections() {
+
+        String[] columnNames = {"State", "Local address", "Local port", "Remote address", "Remote port"};
+        return getTable(new OID(".1.3.6.1.2.1.6.13"), columnNames);
+    }
+
+    @RequestMapping(value = "/get/UdpOpenPorts", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody ResponseEntity<GetTableReply> getUdpOpenPorts() {
+
+        String[] columnNames = {"Local address", "Local port"};
+        return getTable(new OID(".1.3.6.1.2.1.7.5"), columnNames);
+    }
+
+    @RequestMapping(value = "/get/Interfaces", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody ResponseEntity<GetTableReply> getInterfaces() {
+
+        String[] columnNames = {"Description", "Type", "MTU", "Speed", "MAC address",
+                "Administrative Status", "Operational Status", "Last change", "In Octets",
+                "In unicast Packet", "In non-Unicast Packet", "In Discards", "Unknown protocol",
+                "Out Octets", "Out Unicast Packet", "Out non-Unicast Packet", "Out Discards",
+                "Out Queue Lenght", "Doesn't matter"};
+        ResponseEntity<GetTableReply> response = getTable(new OID(".1.3.6.1.2.1.2.2"), columnNames);
+        List<TableRecord> table = response.getBody().getValues();
+        List<TableRecord> newTable = new ArrayList<>();
+        for (TableRecord tableRecord : table) {
+            tableRecord.getFields().remove(tableRecord.getFields().size()-1);
+            newTable.add(new TableRecord(tableRecord.getFields()));
+        }
+        return new ResponseEntity<GetTableReply>(new GetTableReply(response.getBody().getColumnsNames(), newTable),
+                response.getStatusCode());
+    }
+
+    private ResponseEntity<GetTableReply> getTable(OID oid, String[] columnNames)
+    {
+        List<String> columnNamesList= new ArrayList<>();
+
+        for (String name : columnNames) {
+            columnNamesList.add(name);
+        }
+
+        try {
+            Pair<List<List<String>>, List<String>> result = client.getTable(oid);
+            List<List<String>> value = result.getKey();
+            List<TableRecord> table = new ArrayList<>();
+            int columnCount = value.size();
+            int rowCount = value.get(0).size();
+            for (int rowNumber = 0; rowNumber < rowCount; rowNumber++) {
+                List<String> row = new ArrayList<>();
+                for (int columnNumber = 0; columnNumber < columnCount; columnNumber++) {
+                    row.add(value.get(columnNumber).get(rowNumber));
+                }
+                table.add(new TableRecord(row));
+            }
+            return new ResponseEntity<GetTableReply>(new GetTableReply(columnNamesList, table), HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<GetTableReply>(new GetTableReply(null, null), HttpStatus.NO_CONTENT);
+    }
+}
